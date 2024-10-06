4d66bb91c89a5ad602a08411a616c10bb5c7f1c1
reem <reemyousef.cs@gmail.com>
2024-10-06 12:24:28 +0300

    Initial Commit

---
+++ repo1/repo1App.swift
@@ -0,0 +1,17 @@
+//
+//  repo1App.swift
+//  repo1
+//
+//  Created by Reem on 03/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct repo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
